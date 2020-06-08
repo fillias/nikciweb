3c6e224478a3101b03d8c63062f04d8d39300a04
filous <filous.doma@volny.cz>
2020-06-08 14:37:09 +0200

    about page update

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
-      title="About Us"
-      subtitle="Example About Page"
-      image="/uploads/about-hero.jpg"
+      title="O mně"
+      subtitle="Něco o mně"
+      image="/uploads/nikca_rozcuchaná.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
